8ac9870930badbab26ec6960cc90b5ec7f7b77bb
Sailer43 <sitaochen98@gmail.com>
2019-09-07 16:16:02 -0400

    Added several projectile particle effects

---
+++ Assets/Scripts/Editor/AlignmentConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Projectile Data", menuName = "ScriptableObjects/AlignmentConfig", order = 1)]
+public class AlignmentConfig : ScriptableObject
+{
+
+    public string Name;
+    public int index;
+
+}
---
+++ Assets/Scripts/Editor/ProjectileConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName="Projectile Data", menuName="ScriptableObjects/ProjectileConfig", order=1)]
+public class ProjectileConfig : ScriptableObject
+{
+
+    public int Damage;
+    public float Speed;
+
+}
