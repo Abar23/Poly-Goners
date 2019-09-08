85015280ef92c27297d85ad80aeacf7980234f2a
Sailer43 <sitaochen98@gmail.com>
2019-09-08 00:43:52 -0400

    Finished Damage/Health/Projectile System

---
+++ Assets/Scripts/Damage/Config/DamagerConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Damager Data", menuName = "ScriptableObjects/DamagerConfig", order = 1)]
+public class DamagerConfig : ScriptableObject
+{
+
+    public int Damage;
+    public Alignment Alignment;
+
+}
