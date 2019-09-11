253207c072064a8be6e8d0d6600dc50a8d8f2549
Jessica Moyer <jmoyer95@gmail.com>
2019-09-11 15:33:38 -0400

    Added some interactable items to the level

---
+++ Assets/Scripts/Collectibles/Interactable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private Color originalColor;
+    bool mousedOver = false;
+
+    void Update() {
+        if (mousedOver && Input.GetMouseButtonDown(0))
+            Destroy(this.gameObject);
+    }
+
+    void OnMouseEnter() {
+        // highlight object
+        originalColor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = Color.green;
+        mousedOver = true;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+        mousedOver = false;
+    }
+}
