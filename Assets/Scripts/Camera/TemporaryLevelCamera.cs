e4a077579b28fb4ce1fae3d8c8b92cf0b0c0081f
Jessica Moyer <jmoyer95@gmail.com>
2019-09-11 14:52:17 -0400

    Added colliders to objects in level

---
+++ Assets/Scripts/Camera/TemporaryLevelCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporaryLevelCamera : MonoBehaviour
+{
+    public float cameraSpeed;
+
+    // Update is called once per frame
+    void Update() {
+    if(Input.GetKey(KeyCode.RightArrow))
+        transform.Translate(new Vector3(cameraSpeed * Time.deltaTime, 0, 0), Space.World);
+    if(Input.GetKey(KeyCode.LeftArrow))
+        transform.Translate(new Vector3(-cameraSpeed * Time.deltaTime, 0, 0), Space.World);
+    if(Input.GetKey(KeyCode.DownArrow))
+        transform.Translate(new Vector3(0, 0, -cameraSpeed * Time.deltaTime), Space.World);
+    if(Input.GetKey(KeyCode.UpArrow))
+        transform.Translate(new Vector3(0, 0, cameraSpeed * Time.deltaTime), Space.World);    
+    }
+}
