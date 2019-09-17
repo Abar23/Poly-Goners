39f8a349eaeab7d73d91c323f3478b16e002f463
Sailer43 <sitaochen98@gmail.com>
2019-09-16 23:29:29 -0400

    Make enemy searching compatible with 2 players

--- Assets/Scripts/Enemies/Skeleton/Skeleton.cs
+++ Assets/Scripts/Enemies/Skeleton/Skeleton.cs
@@ -8,21 +8,27 @@ public class Skeleton : MonoBehaviour
 {
 
     [SerializeField] private TargetScanner m_Scanner;
-    private static Transform m_Player;
+    private static List<Transform> m_Players;
     [SerializeField] private float m_AttackDelay = 3.0f;
 
     private NavMeshAgent m_Agent;
     private SkeletonAnimatorController m_Controller;
     private const float k_ScanInterval = 0.5f;
+    private int followingIndex;
     
 
     void Awake()
     {
         m_Agent = GetComponent<NavMeshAgent>();
         m_Controller = GetComponent<SkeletonAnimatorController>();
-        if (m_Player == null)
+        if (m_Players == null)
         {
-            m_Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            m_Players = new List<Transform>();
+            GameObject[] objects = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject ob in objects)
+            {
+                m_Players.Add(ob.transform);
+            }
         }
     }
 
@@ -35,10 +41,14 @@ public class Skeleton : MonoBehaviour
     {
         while (true)
         {
-            if (m_Scanner.Detect(transform, m_Player))
+            for (int i = 0; i < m_Players.Count; i++)
             {
-                StartCoroutine(ReachForPlayer());
-                break;
+                if (m_Scanner.Detect(transform, m_Players[i]))
+                {
+                    followingIndex = i;
+                    StartCoroutine(ReachForPlayer());
+                    break;
+                }
             }
             yield return new WaitForSeconds(k_ScanInterval);
         }
@@ -47,12 +57,12 @@ public class Skeleton : MonoBehaviour
     IEnumerator ReachForPlayer(float delay = 0f)
     {
         yield return new WaitForSeconds(delay);
-        m_Agent.SetDestination(m_Player.position);
+        m_Agent.SetDestination(m_Players[followingIndex].position);
         EnableWalk(true);
         while (true)
         {
-            m_Agent.SetDestination(m_Player.position);
-            if (CalculateDistanceToPlayer() <= m_Agent.stoppingDistance)
+            m_Agent.SetDestination(m_Players[followingIndex].position);
+            if (CalculateDistanceToPlayer(followingIndex) <= m_Agent.stoppingDistance)
             {
                 EnableWalk(false);
                 m_Controller.Attack(true);
@@ -63,9 +73,9 @@ public class Skeleton : MonoBehaviour
         }
     }
 
-    float CalculateDistanceToPlayer()
+    float CalculateDistanceToPlayer(int i)
     {
-        Vector3 toPlayer = transform.position - m_Player.position;
+        Vector3 toPlayer = transform.position - m_Players[i].position;
         toPlayer.y = 0;
         return toPlayer.magnitude;
     }
