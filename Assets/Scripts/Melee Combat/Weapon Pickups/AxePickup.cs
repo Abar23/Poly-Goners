65d91fb3a17917382d073d4296f300fb10b1958a
Jessica Moyer <jmoyer95@gmail.com>
2019-09-25 14:43:20 -0400

    Initial implementation of axe

---
+++ Assets/Scripts/Melee Combat/Weapon Pickups/AxePickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxePickup : MonoBehaviour
+{
+    private WeaponManager weaponManager;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            weaponManager = other.gameObject.GetComponentInChildren<WeaponManager>();
+            if (weaponManager != null) 
+            {
+                weaponManager.EquipAxe();
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/Melee Combat/WeaponManager.cs
+++ Assets/Scripts/Melee Combat/WeaponManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class WeaponManager : MonoBehaviour
 {
     private GameObject sword;
+    private GameObject axe;
 
     void Start()
     {
-        // weapon indices must correspond to child number for GetChild
+        // weapon indices must correspond to child number for GetChild (i.e. sword is the first child so use index 0)
         sword = this.gameObject.transform.GetChild(0).gameObject;
+        axe = this.gameObject.transform.GetChild(1).gameObject;
     }
 
     public void EquipSword()
@@ -20,5 +22,15 @@ public class WeaponManager : MonoBehaviour
     public void UnequipSword()
     {
         sword.SetActive(false);
+    }
+
+    public void EquipAxe()
+    {
+        axe.SetActive(true);
+    }
+
+    public void UnequipAxe()
+    {
+        axe.SetActive(false);
     }
 }
