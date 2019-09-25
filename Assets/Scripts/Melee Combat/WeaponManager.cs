19a901f0a897fc1645ea61143970e5881d22dbd5
Jessica Moyer <jmoyer95@gmail.com>
2019-09-25 14:15:47 -0400

    Added weapon manager to equip/unequip weapons

---
+++ Assets/Scripts/Melee Combat/WeaponManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    private GameObject sword;
+
+    void Start()
+    {
+        // weapon indices must correspond to child number for GetChild
+        sword = this.gameObject.transform.GetChild(0).gameObject;
+    }
+
+    public void EquipSword()
+    {
+        sword.SetActive(true);
+    }
+
+    public void UnequipSword()
+    {
+        sword.SetActive(false);
+    }
+}
