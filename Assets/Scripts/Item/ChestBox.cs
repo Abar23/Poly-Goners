6f3d2923bfe0a1eb8642ffa7e86e82e90579aeac
sailer43 <sailer_cst@yahoo.com>
2019-09-27 13:03:17 -0400

    Made chests in treasure room interactable

--- Assets/Scripts/Item/ChestBox.cs
+++ Assets/Scripts/Item/ChestBox.cs
@@ -6,6 +6,7 @@ public class ChestBox : MonoBehaviour
 {
 
     [SerializeField] private GameObject m_Lid;
+    [SerializeField] private Vector3 m_Axis;
 
     private const float k_openAngle = 60f;
     private const int k_numSteps = 50;
@@ -25,7 +26,7 @@ public class ChestBox : MonoBehaviour
         float step_angle = k_openAngle / k_numSteps;
         for (int step = 0; step < k_numSteps; step++)
         {
-            m_Lid.transform.Rotate(-Vector3.right, step_angle);
+            m_Lid.transform.Rotate(m_Axis, step_angle);
             yield return new WaitForEndOfFrame();
         }
     }
