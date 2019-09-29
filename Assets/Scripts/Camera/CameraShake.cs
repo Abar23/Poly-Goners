d582c080ecf63b43f1fa14baf53f248ca11cdb1f
Sailer43 <sitaochen98@gmail.com>
2019-09-29 19:58:21 -0400

    Implemented camera shake

---
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    
+    public IEnumerator CameraShakeEffect(float maganitude, float duration)
+    {
+        
+        float timeElapse = 0f;
+        while (timeElapse < duration)
+        {
+            float x = Random.Range(-maganitude, maganitude);
+            float y = Random.Range(-maganitude, maganitude);
+            float z = Random.Range(-maganitude, maganitude);
+
+            transform.position = new Vector3(x, y, z) + transform.position;
+            timeElapse += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+}
