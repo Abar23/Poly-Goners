8d7f616d72e085ecb3aec921d7ccf0faa9b0ca55
Jessica Moyer <jmoyer95@gmail.com>
2019-10-13 17:44:44 -0400

    Implemented player 2 being able to buy items from shop

--- Assets/Scripts/Shops/ShopItem.cs
+++ Assets/Scripts/Shops/ShopItem.cs
@@ -29,14 +29,27 @@ public class ShopItem : MonoBehaviour
         float distanceFromPlayer1 = Vector3.Distance(transform.position, player1.transform.position);
         float distanceFromPlayer2 = Vector3.Distance(transform.position, player2.transform.position);
 
-        if (distanceFromPlayer1 <= promptActivationDistance)
+
+        if (distanceFromPlayer1 <= promptActivationDistance || distanceFromPlayer2 <= promptActivationDistance)
         {
             buyPrompt.SetActive(true);
             itemName.enabled = true;
 
-            if (player1.GetComponent<Inventory>().GetGold() >= price && player1.GetComponent<Player>().CheckUseButtonPress())
-                BuyItem();
+            // Check if Player 1 buys item
+            if (distanceFromPlayer1 <= promptActivationDistance)
+            {
+                if (player1.GetComponent<Inventory>().GetGold() >= price && player1.GetComponent<Player>().CheckUseButtonPress())
+                    BuyItem(player1);
+            }
+
+            // Check if Player 2 buys item
+            else if (distanceFromPlayer2 <= promptActivationDistance)
+            {
+                if (player2.GetComponent<Inventory>().GetGold() >= price && player2.GetComponent<Player>().CheckUseButtonPress())
+                    BuyItem(player2);
+            }
         }
+
         else
         {
             buyPrompt.SetActive(false);
@@ -44,9 +57,9 @@ public class ShopItem : MonoBehaviour
         }
     }
 
-    void BuyItem()
+    void BuyItem(GameObject player)
     {
-        player1.GetComponent<Inventory>().DecreaseGold(price);
+        player.GetComponent<Inventory>().DecreaseGold(price);
         
         // TODO: add to player's inventory
 
