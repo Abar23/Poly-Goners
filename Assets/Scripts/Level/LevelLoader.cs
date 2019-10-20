dc0fa38a7471665772e3c608bf60a4282953e052
Jessica Moyer <jmoyer95@gmail.com>
2019-10-20 18:42:19 -0400

    Implemented level loader and level factory for managing level loading

---
+++ Assets/Scripts/Level/LevelLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public enum Level { HubWorld, DungeonLevel1 };
+    public Level level;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("player in portal");
+
+        if (level != null) {
+            if (other.gameObject.tag == "Player") {
+                LevelFactory.LoadLevel(level);
+            }       
+        }
+    }
+}
