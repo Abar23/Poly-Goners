c5fd84783feeaa7e2da4b0709b214f9472d0d9b3
Jessica Moyer <jmoyer95@gmail.com>
2019-10-20 21:53:09 -0400

    Implemented lose state

---
+++ Assets/Scripts/UI/GameOverMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverMenuManager : MonoBehaviour
+{
+    public GameObject gameOverMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverMenu.SetActive(false);
+    }
+
+    public void DisplayGameOverMenu() {
+        gameOverMenu.SetActive(true);
+    }
+}
