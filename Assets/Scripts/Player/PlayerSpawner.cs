e3c7a6f3f3eae9d3aab3b30b179c0710aef695ad
kylelambert <kylelambertdev@gmail.com>
2019-10-23 01:10:57 -0400

    fixed bug maybe ?? hopefully lmao

--- Assets/Scripts/Player/PlayerSpawner.cs
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -2,22 +2,27 @@
 
 public class PlayerSpawner : MonoBehaviour
 {
+    private bool playerIsSet = false;
     public bool isPlayerOne;
 
-    void Start()
+    void FixedUpdate()
     {
-        GameObject playerOne = PlayerManager.GetInstance().GetPlayerOneGameObject();
-        GameObject playerTwo = PlayerManager.GetInstance().GetPlayerTwoGameObject();
+        if (this.playerIsSet == false)
+        {
+            GameObject playerOne = PlayerManager.GetInstance().GetPlayerOneGameObject();
+            GameObject playerTwo = PlayerManager.GetInstance().GetPlayerTwoGameObject();
 
-        if (this.isPlayerOne)
-        {
-            playerOne.transform.position = this.transform.position;
-            playerOne.transform.rotation = this.transform.rotation;
-        }
-        else
-        {
-            playerTwo.transform.position = this.transform.position;
-            playerTwo.transform.rotation = this.transform.rotation;
+            if (this.isPlayerOne)
+            {
+                playerOne.transform.position = this.transform.position;
+                playerOne.transform.rotation = this.transform.rotation;
+            }
+            else
+            {
+                playerTwo.transform.position = this.transform.position;
+                playerTwo.transform.rotation = this.transform.rotation;
+            }
+            this.playerIsSet = true;
         }
     }
 }
