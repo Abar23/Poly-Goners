925829ec5857c2b9ba1b8afca5c6eb1cc9c47730
sailer43 <sitaochen98@gmail.com>
2019-10-29 02:17:42 -0400

    Implemented door locking system

---
+++ Assets/Scripts/Level/Door.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]private bool m_IsOpen = false;
+    [SerializeField] private float m_OpenAngle;
+    private const int k_RotationInterval = 30;
+    private bool isMoving = false;
+
+    public void Open()
+    {
+        if (!m_IsOpen && !isMoving)
+        {
+            isMoving = true;
+            StartCoroutine(OpenDoor());
+        }
+    }
+
+    public void Close()
+    {
+        if (m_IsOpen && !isMoving)
+        {
+            isMoving = true;
+            StartCoroutine(CloseDoor());
+        }
+    }
+    IEnumerator OpenDoor()
+    {
+        for (int i = 0; i < k_RotationInterval; i++)
+        {
+            transform.Rotate(Vector3.up * m_OpenAngle / k_RotationInterval);
+            yield return new WaitForFixedUpdate();
+        }
+        m_IsOpen = true;
+        isMoving = false;
+    }
+
+    IEnumerator CloseDoor()
+    {
+        for (int i = 0; i < k_RotationInterval; i++)
+        {
+            transform.Rotate(Vector3.up * m_OpenAngle / -k_RotationInterval);
+            yield return new WaitForFixedUpdate();
+        }
+        m_IsOpen = false;
+        isMoving = false;
+    }
+}
---
+++ Assets/Scripts/Level/DoorController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] private Door m_Door;
+    private List<Player> playersInside;
+    private int num_player;
+
+    void Awake()
+    {
+        playersInside = new List<Player>();
+    }
+
+    void Update()
+    {
+        num_player = GameObject.FindGameObjectsWithTag("Player").Length;
+        if (playersInside.Count == num_player)
+        {
+            m_Door.Open();
+        }
+        else
+        {
+            m_Door.Close();
+        }
+        Debug.Log("Number of Player = " + num_player);
+        Debug.Log("Number of Player Inside = " + playersInside.Count);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            if (player == null || other.isTrigger)
+            {
+                return;
+            }
+            if (!playersInside.Contains(player))
+            {
+                playersInside.Add(player);
+                
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            if (player == null || other.isTrigger)
+            {
+                return;
+            }
+            if (playersInside.Contains(player))
+            {
+                playersInside.Remove(player);
+                Debug.Log(other.gameObject);
+            }
+        }
+    }
+}
