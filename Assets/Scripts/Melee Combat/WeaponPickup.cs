bab2ffc04a6d396a626ea09d59425b3302d265e7
Jessica Moyer <jmoyer95@gmail.com>
2019-10-30 15:26:49 -0400

    Added pick up prompt for weapon pickups

--- Assets/Scripts/Melee Combat/WeaponPickup.cs
+++ Assets/Scripts/Melee Combat/WeaponPickup.cs
@@ -1,17 +1,72 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponPickup : MonoBehaviour
 {
     private WeaponManager weaponManager;
+    private float promptActivationDistance = .75f;
+    private GameObject player1;
+    private GameObject player2;
+    public GameObject textPanel;
+    public Text inventoryFullText; 
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
+    void Start() {
+        player1 = PlayerManager.GetInstance().GetPlayerOneGameObject();
+        player2 = PlayerManager.GetInstance().GetPlayerTwoGameObject();
+
+        Debug.Log(player1);
+        textPanel.SetActive(false);
+        inventoryFullText.gameObject.SetActive(false);
+    }
+
+    void Update() {
+        float distanceFromPlayer1 = Vector3.Distance(transform.position, player1.transform.position);
+        float distanceFromPlayer2 = Vector3.Distance(transform.position, player2.transform.position);
+
+
+        if (distanceFromPlayer1 <= promptActivationDistance || distanceFromPlayer2 < promptActivationDistance)
         {
-            Inventory inv = other.GetComponent<Inventory>();
+            textPanel.SetActive(true);
+
+            // Check if Player 1 buys item
+            if (distanceFromPlayer1 <= promptActivationDistance)
+            {
+                if (player1.GetComponent<Player>().CheckUseButtonPress()) {
+                    if (!player1.GetComponent<Inventory>().IsMeleeFull()) {
+                        EquipWeapon(player1);
+                    }
+                    else {
+                        inventoryFullText.gameObject.SetActive(true);
+                    }
+                }       
+            }
+
+            // Check if Player 2 buys item
+            else if (distanceFromPlayer2 <= promptActivationDistance)
+            {
+                if (player2.GetComponent<Player>().CheckUseButtonPress()) {
+                    if (!player2.GetComponent<Inventory>().IsMeleeFull()) {
+                        EquipWeapon(player2);
+                    }
+                    else {
+                        inventoryFullText.gameObject.SetActive(true);
+                    }
+                }  
+            }
+        }
+
+        else
+        {
+            textPanel.SetActive(false);
+            inventoryFullText.gameObject.SetActive(false);
+        }
+    }
+
+    void EquipWeapon(GameObject player) {
+            Inventory inv = player.GetComponent<Inventory>();
             if (!inv.IsMeleeFull())
             {
-                weaponManager = other.gameObject.GetComponentInChildren<WeaponManager>();
+                weaponManager = player.gameObject.GetComponentInChildren<WeaponManager>();
                 if (weaponManager != null)
                 {
                     inv.AddMeleeWeapon(weaponManager.weaponPickups[this.gameObject.name].GetComponent<Weapon>(), this.gameObject);
@@ -19,6 +74,5 @@ public class WeaponPickup : MonoBehaviour
                     this.gameObject.SetActive(false);
                 }
             }
-        }
     }
 }
---
+++ Assets/Scripts/UI/ItemLabel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemLabel : MonoBehaviour
+{
+    private GameObject player1;
+    private GameObject player2;
+    public GameObject textPanel;
+    float promptActivationDistance = .75f;
+
+    void Start() {
+        player1 = PlayerManager.GetInstance().GetPlayerOneGameObject();
+        player2 = PlayerManager.GetInstance().GetPlayerTwoGameObject();
+        textPanel.SetActive(false);
+    }
+
+    void Update() {
+        float distanceFromPlayer1 = Vector3.Distance(transform.position, player1.transform.position);
+        float distanceFromPlayer2 = Vector3.Distance(transform.position, player2.transform.position);
+
+        if (distanceFromPlayer1 <= promptActivationDistance || distanceFromPlayer2 < promptActivationDistance) {
+            textPanel.SetActive(true);
+        } else {
+            textPanel.SetActive(false);
+        }
+    }
+}
