f57d012a08a349acaf51c295c04a7c1b9ddadc7e
Jessica Moyer <jmoyer95@gmail.com>
2019-11-18 04:56:42 -0500

    Items can now be picked up from item pedestals

--- Assets/Scripts/Shops/UnlockPedestal.cs
+++ Assets/Scripts/Shops/UnlockPedestal.cs
@@ -5,23 +5,72 @@ using UnityEngine;
 public class UnlockPedestal : MonoBehaviour
 {
     private bool isActive;
-    private GameObject unlockType;
+    private GameObject pickupObject;
     private GameObject displayObject;
+    private GameObject player1;
+    private GameObject player2;
+    private float pickupDistance = 2.5f;
+    private GameObject createdPickup;
     
     void Start() {
         isActive = false;
-        unlockType = transform.GetChild(0).gameObject;
+        pickupObject = transform.GetChild(0).gameObject;
         displayObject = transform.GetChild(1).gameObject;
+        player1 = PlayerManager.GetInstance().GetPlayerOneGameObject();
+        player2 = PlayerManager.GetInstance().GetPlayerTwoGameObject();
     }
 
     void Update() {
         if (!isActive) {
             displayObject.SetActive(false);
-            isActive = ItemUnlockManager.instance.CheckIfItemIsActive(unlockType.name);
+            isActive = ItemUnlockManager.instance.CheckIfItemIsActive(pickupObject.name);
         }
 
         if (isActive) {
             displayObject.SetActive(true);
+
+            float distanceFromPlayer1 = Vector3.Distance(transform.position, player1.transform.position);
+            float distanceFromPlayer2 = Vector3.Distance(transform.position, player2.transform.position);
+
+            if (distanceFromPlayer1 <= pickupDistance || distanceFromPlayer2 < pickupDistance) {
+                if (distanceFromPlayer1 <= pickupDistance) {
+                    if (createdPickup == null) {
+                        CreateNewPickup(player1.transform.position);
+                    } 
+
+                    else {
+                        createdPickup.transform.position = player1.transform.position;
+                        
+                        if (player1.GetComponent<Player>().CheckUseButtonPress()) {
+                            createdPickup.GetComponentInChildren<MeshRenderer>().enabled = true;
+                        }
+                    }
+                }
+
+                else if (distanceFromPlayer2 <= pickupDistance) {
+                    if (createdPickup == null) {
+                        CreateNewPickup(player2.transform.position);
+                    } 
+
+                    else {
+                        createdPickup.transform.position = player2.transform.position;
+                        
+                        if (player2.GetComponent<Player>().CheckUseButtonPress()) {
+                            createdPickup.GetComponentInChildren<MeshRenderer>().enabled = true;
+                        }
+                    }
+                }
+            }
         }
     }
+
+    private void CreateNewPickup(Vector3 playerPosition) {
+        createdPickup = Instantiate(pickupObject, playerPosition, Quaternion.identity);
+        createdPickup.name = pickupObject.name;
+        createdPickup.GetComponentInChildren<MeshRenderer>().enabled = false;
+        createdPickup.SetActive(true);
+        createdPickup.GetComponentInChildren<ParticleSystem>().Stop();
+        createdPickup.GetComponentInChildren<ParticleSystem>().Clear();
+    }
 }
+
