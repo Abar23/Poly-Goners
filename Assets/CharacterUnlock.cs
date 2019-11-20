9c9c5c9c9687c2abab2697e7ff9a5c96b0b50bdf
kylelambert <kylelambertdev@gmail.com>
2019-11-20 04:14:47 -0500

    finished jail room

--- Assets/CharacterUnlock.cs
+++ Assets/CharacterUnlock.cs
@@ -12,8 +12,10 @@ public class CharacterUnlock : MonoBehaviour
     private GameObject player1;
     private GameObject player2;
     private GameObject textPanel;
+    private GameObject notEnoughGoldText;
+    private GameObject activeChar;
     private float promptActivationDistance = 1f;
-
+    private ParticleSystem ps;
     private int subCharIndex = 0;
 
     void Start()
@@ -23,12 +25,18 @@ public class CharacterUnlock : MonoBehaviour
 
         textPanel = GetComponentInChildren<Canvas>().transform.GetChild(0).gameObject;
         textPanel.gameObject.transform.GetChild(0).transform.gameObject.GetComponent<Text>().text = "Cost: " + Price + " Gold";
+        notEnoughGoldText = textPanel.transform.GetChild(5).gameObject;
+        notEnoughGoldText.SetActive(true);
         textPanel.SetActive(false);
+        ps = GetComponentInChildren<ParticleSystem>();
+        if (ps != null)
+            ps.gameObject.SetActive(false);
 
         for (int i = 1; i < gameObject.transform.childCount; i++)
         {
             if (gameObject.transform.GetChild(i).gameObject.activeSelf == true)
             {
+                activeChar = gameObject.transform.GetChild(i).gameObject;
                 subCharIndex = i - 1;
                 break;
             }
@@ -37,44 +45,78 @@ public class CharacterUnlock : MonoBehaviour
 
     void Update()
     {
-        float distanceFromPlayer1 = Vector3.Distance(CellDoor.transform.position, player1.transform.position);
-        float distanceFromPlayer2 = Vector3.Distance(CellDoor.transform.position, player2.transform.position);
-
-        if (distanceFromPlayer1 <= promptActivationDistance || distanceFromPlayer2 <= promptActivationDistance)
+        if (activeChar.activeSelf)
         {
-            // Display menu to player 1
-            if (distanceFromPlayer1 <= promptActivationDistance)
+            float distanceFromPlayer1 = Vector3.Distance(CellDoor.transform.position, player1.transform.position);
+            float distanceFromPlayer2 = Vector3.Distance(CellDoor.transform.position, player2.transform.position);
+
+            if (distanceFromPlayer1 <= promptActivationDistance || distanceFromPlayer2 <= promptActivationDistance)
             {
-                if (player1.GetComponent<Inventory>().GetGold() >= Price && player1.GetComponent<Player>().CheckUseButtonPress())
+                // Display menu to player 1
+                if (distanceFromPlayer1 <= promptActivationDistance)
                 {
-                    player1.GetComponent<CharacterBox>().SetCharacter(CharacterIndex, subCharIndex);
-                    player1.GetComponent<Player>().UpdateAnimator();
-                    player1.GetComponent<Inventory>().DecreaseGold(Price);
+                    if (player1.GetComponent<Player>().CheckUseButtonPress())
+                    {
+                        if (player1.GetComponent<Inventory>().GetGold() >= Price)
+                        {
+                            player1.GetComponent<CharacterBox>().SetCharacter(CharacterIndex, subCharIndex);
+                            player1.GetComponent<Player>().UpdateAnimator();
+                            player1.GetComponent<Inventory>().DecreaseGold(Price);
+                            //CellDoor.SetActive(false);
+                            if (ps != null)
+                                ps.gameObject.SetActive(true);
+                            textPanel.SetActive(false);
+                            activeChar.SetActive(false);
+                            
+                        }
+                        else
+                        {
+                            notEnoughGoldText.SetActive(true);
+                        }
+                    }
+
+                    textPanel.SetActive(true);
+                    textPanel.transform.position = new Vector3(transform.position.x, player1.transform.position.y + 3.5f, transform.position.z);
                 }
 
-                textPanel.SetActive(true);
-                textPanel.transform.position = new Vector3(transform.position.x, player1.transform.position.y + 3.5f, transform.position.z);
-            }
-
-            // Display menu to player 2
-            else if (distanceFromPlayer2 <= promptActivationDistance)
-            {
-                if (player2.GetComponent<Inventory>().GetGold() >= Price && player2.GetComponent<Player>().CheckUseButtonPress())
+                // Display menu to player 2
+                else if (distanceFromPlayer2 <= promptActivationDistance)
                 {
-                    player2.GetComponent<CharacterBox>().SetCharacter(CharacterIndex, subCharIndex);
-                    player2.GetComponent<Player>().UpdateAnimator();
-                    player2.GetComponent<Inventory>().DecreaseGold(Price);
-                }
+                    if (player2.GetComponent<Player>().CheckUseButtonPress())
+                    {
+                        if (player2.GetComponent<Inventory>().GetGold() >= Price)
+                        {
+                            player2.GetComponent<CharacterBox>().SetCharacter(CharacterIndex, subCharIndex);
+                            player2.GetComponent<Player>().UpdateAnimator();
+                            player2.GetComponent<Inventory>().DecreaseGold(Price);
+                            //CellDoor.SetActive(false);
+                            if (ps != null)
+                                ps.gameObject.SetActive(true);
+                            textPanel.SetActive(false);
+                            activeChar.SetActive(false);
+                        }
+                        else
+                        {
+                            notEnoughGoldText.SetActive(true);
+                        }
+                    }
 
-                textPanel.SetActive(true);
-                textPanel.transform.position = new Vector3(transform.position.x, player2.transform.position.y + 3.5f, transform.position.z);
+                    textPanel.SetActive(true);
+                    textPanel.transform.position = new Vector3(transform.position.x, player2.transform.position.y + 3.5f, transform.position.z);
+                }
+                else
+                {
+                    notEnoughGoldText.SetActive(false);
+                    textPanel.SetActive(false);
+                }
             }
+
             else
             {
+                notEnoughGoldText.SetActive(false);
                 textPanel.SetActive(false);
             }
         }
-
         else
         {
             textPanel.SetActive(false);
