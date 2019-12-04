90d7470950ae529592a328745d7df4ef69b3a7fe
Anthony Bartholomew <abar15.abb@gmail.com>
2019-12-04 02:09:35 -0500

    Dungeon reload improvement

--- Assets/Scripts/Dungeon Generation/DungeonReloader.cs
+++ Assets/Scripts/Dungeon Generation/DungeonReloader.cs
@@ -14,7 +14,6 @@ public class DungeonReloader : MonoBehaviour
         if(other.tag == "Player")
         {
             DungeonCompletionTracker.GetInstance().IncreaseNumberOfCompletedDungeons();
-            OnReload.Invoke();
             StartCoroutine(ReloadScene());
         }
     }
@@ -29,6 +28,7 @@ public class DungeonReloader : MonoBehaviour
         collider.enabled = false;
         animator.SetTrigger("Exit");
         yield return new WaitForSeconds(k_ExitTime);
+        OnReload.Invoke();
         op.allowSceneActivation = true;
     }
 }
