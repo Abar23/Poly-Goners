314d3574a020344d63e57f370f206488c48f4813
Anthony Bartholomew <abar15.abb@gmail.com>
2019-12-05 03:11:11 -0500

    Updated Drop Script and Dragons Drop Magic

--- Assets/Scripts/Enemies/Skeleton/EnemyDrop.cs
+++ Assets/Scripts/Enemies/Skeleton/EnemyDrop.cs
@@ -18,15 +18,21 @@ public class EnemyDrop : MonoBehaviour
 
     public void DropItem()
     {
+        List<DropableItem> weaponsToSpawn = new List<DropableItem>();
         float spawnChance = UnityEngine.Random.Range(0f, 1f);
         foreach (DropableItem item in DropList)
         {
             if (spawnChance < item.SpawnChance)
             {
-                GameObject newObj = Instantiate(item.Object, transform.position, Quaternion.identity);
-                newObj.name = newObj.name.Substring(0, newObj.name.Length - 7);
-                break;
+                weaponsToSpawn.Add(item);
             }
+        }
+
+        if (weaponsToSpawn.Count > 0)
+        {
+            DropableItem item = weaponsToSpawn[UnityEngine.Random.Range(0, weaponsToSpawn.Count)];
+            GameObject newObj = Instantiate(item.Object, transform.position, Quaternion.identity);
+            newObj.name = newObj.name.Substring(0, newObj.name.Length - 7);
         }
     }
 }
